af2226e828325b20603c0bd889dac035fafcf471
Rahman <rahman9600@gmail.com>
2019-05-25 10:28:04 +0700

    Create Dictionary.py
    init Dictionary.py & isinya

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+# Dictionary.py
+
+fruit = {"Apple":8, "Jeruk":6, "Melon":11}
+print(fruit)
+print(fruit['Apple'])
+
+new_dict = {"key1":15, "key2":[19, 79, 20], "key3":{"Apple":8}}
+print(new_dict)
+print(new_dict["key1"])
+print(new_dict["key2"][1])
+print(new_dict["key3"])
+print(new_dict["key3"]["Apple"])
