3f07d57409ea1a3effdebe191c5480bbbf9284e3
Rahman <rahman9600@gmail.com>
2019-05-25 10:27:41 +0700

    Create List.py
    isi List.py

---
+++ List.py
@@ -0,0 +1,24 @@
+# List.py
+
+first_list = ["Salam", 10, 17.79]
+print("first_list", first_list)
+
+second_list = ["Budi", "Luhur", "Sakti"]
+print("second_list", second_list)
+
+print("Gabungan first & second list ", first_list, second_list)
+
+new_list = first_list + second_list
+print("new_list", new_list)
+
+empty_list = []
+print("empty_list", empty_list)
+
+ahs = ["Budi", "Luhur", "Sakti"]
+print("array ahs", ahs)
+print("ahs ke 1",ahs[0])
+print("ahs ke 1 dari belakang", ahs[-1])
+print("ahs dari 0 sampai 1", ahs[0:2])
+
+ahs.append("Baru")
+print(ahs)
