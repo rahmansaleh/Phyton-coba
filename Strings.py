44f307975379d466fae3287f5895cde33d5aafa1
Rahman <rahman9600@gmail.com>
2019-05-25 10:07:21 +0700

    Create Strings.py
    initial Strings.py dan isinya

---
+++ Strings.py
@@ -0,0 +1,27 @@
+# String.py
+
+greeting = "Hello"
+name = "Budi Luhur"
+
+print(greeting + name)
+print(greeting + " " + name)
+
+# --------------
+
+greeting = "Hello"
+name = input("Inputkan Nama Anda : ")
+
+print(greeting +" " + name)
+
+# -------------
+
+split_string = "Hello, Budi Luhur"
+print(split_string)
+
+split_string = "Hello, \nBudi Luhur"
+print(split_string)
+
+# -------------
+tab_string = "1\t2\t3"
+print(tab_string)
+
