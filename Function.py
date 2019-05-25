b0808801e7f578875de9a29ccd368f86e79aad9f
Rahman <rahman9600@gmail.com>
2019-05-25 11:56:36 +0700

    Update Function.py

--- Function.py
+++ Function.py
@@ -2,13 +2,17 @@
 # tips : sebaiknya function hanya mengembalikan nilai. tidak mencetak aoutput
 
 def hello(name="Budi Luhur"):
+    """
+    Created By : Rahman
+    Input : 1 - String
+    Output : 1 - String
+    """
     return "Hello, "+name
 
 def hitung(a=5, b=4):
     return a*b
 
+print(hello())
 print(hello("rahman"))
+print(hitung())
 print(hitung(1,2))
-
-
-# hello()
